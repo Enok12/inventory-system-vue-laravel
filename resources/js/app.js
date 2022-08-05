e41ad43d30cb66d26edbef666d37f7fea782f543
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-08-05 19:47:38 +0530

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+
+
+require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//Router import
+import {routes} from './routes';
+
+//import user Class
+import User from './Helpers/User';
+window.User = User;
+
+//Sweet Alert
+import Swal from 'sweetalert2'
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
+
+window.Toast = Toast;
+
+
+const router = new VueRouter({
+    routes, // short for `routes: routes`
+    mode:'history'
+  })
+
+const app = new Vue({
+    el: '#app',
+    router
+});
