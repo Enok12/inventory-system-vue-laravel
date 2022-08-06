1e81d4edac9c1dcc4f1212cb1faae6c1be7910fc
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-08-06 09:54:24 +0530

    Register and logout Development

---
+++ resources/js/components/auth/logout.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+
+export default{
+    created(){
+         localStorage.removeItem('token');
+        localStorage.removeItem('user');
+
+         //Toast Message
+      Toast.fire({
+      icon: 'success',
+      title: 'Logout Successfuly'
+      })
+
+      this.$router.push({name:'/'})
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,6 +2,8 @@ let login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 let forget = require('./components/auth/forget.vue').default;
 let home = require('./components/home.vue').default;
+let logout = require('./components/auth/logout.vue').default;
+
 
 
 
@@ -11,4 +13,6 @@ export const routes = [
     { path: '/register', component: register,name:'/register' },
     { path: '/forget', component: forget,name:'/forget' },
     { path: '/home', component: home,name:'/home' },
+    { path: '/logout', component: logout,name:'/logout' },
+
   ]
