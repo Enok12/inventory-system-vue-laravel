5ef3b8dad9e6f85adb8cadead90c7711ca648145
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-08-08 09:20:07 +0530

    All Employee

--- resources/js/components/employee/index.vue
+++ resources/js/components/employee/index.vue
@@ -3,33 +3,50 @@
         <div class="row">
             <router-link to="store-employee" class="btn btn-primary">Add Employee</router-link>
         </div>
-        <div class="row justify-content-center">
-             
-      <div class="col-xl-12 col-lg-12 col-md-12">
-        <div class="card shadow-sm my-5">
-          <div class="card-body p-0">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="login-form">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">All Employee</h1>
-                  </div>
-               
-                  <hr>
-                  <div class="text-center">
-    
-
-
-                  </div>
-                  <div class="text-center">
-                  </div>
+        <br><br>
+        
+ <div class="row">
+            <div class="col-lg-12 mb-4">
+              <!-- Simple Tables -->
+              <div class="card">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Employee List</h6>
                 </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>Name</th>
+                        <th>Photo</th>
+                        <th>Phone</th>
+                        <th>Salary</th>
+                        <th>Joining Date</th>
+                         <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      
+                      <tr v-for="employee in employees" :key="employee.id">
+                        <td>{{employee.name}}</td>
+                        <td><img :src="employee.photo" alt="" id="em_photo"></td>
+                        <td>{{employee.phone}}</td>
+                        <td>{{employee.salary}}</td>
+                        <td>{{employee.joining_date}}</td>
+                        <td><a href="#" class="btn btn-sm btn-primary">Edit</a>
+                        <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                      </tr>
+                    
+                    </tbody>
+                  </table>
+                </div>
+                <div class="card-footer"></div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+
+
+
     </div>
 </template>
 
@@ -43,13 +60,27 @@ created(){
     this.$router.push({name:'/home'})
   }
 },
-
+data(){
+  return {
+    employees:[]
+  }
+},
 methods:{
-    
+    allemployee(){
+      axios.get('./api/employeee')
+      .then(({data}) => (this.employees = data))
+      .catch()
+    }
+},
+created(){
+  this.allemployee();
 }
 }
 </script>
 
 <style scoped>
-
+#em_photo{
+  width: 50px;
+  height: 50px;
+}
 </style>
