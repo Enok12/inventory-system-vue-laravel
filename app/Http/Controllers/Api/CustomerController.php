0bec8f3f6f1128cdb0c8542f8f4ebbed3003ca41
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-08-19 10:09:43 +0530

    Update Customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -86,7 +86,8 @@ class CustomerController extends Controller
      */
     public function show($id)
     {
-        //
+        $customer = DB::table('customers')->where('id',$id)->first();
+        return response()->json($customer);
     }
 
     /**
@@ -109,7 +110,38 @@ class CustomerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = array();
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['phone'] = $request->phone;
+        $data['address'] = $request->address;
+        
+        $image = $request->newphoto;
+
+        if($image){
+            $position = strpos($image,';');
+            $sub = substr($image,0,$position);
+            $exi = explode('/',$sub)[1];
+
+            $name = time().".".$exi;
+            $img = Image::make($image)->resize(240,200);
+            $upload_path = 'backend/customer/';
+            $img_url = $upload_path.$name;
+            $success = $img->save($img_url);
+
+            if($success){
+                $data['photo'] = $img_url;
+                $img = DB::table('customers')->where('id',$id)->first();
+                $image_path = $img->photo;
+                $done = unlink($image_path);
+                $user = DB::table('customers')->where('id',$id)->update($data); 
+            }
+        }else{
+            $oldphoto = $request->photo;
+            $data['photo'] = $oldphoto;
+            $user = DB::table('customers')->where('id',$id)->update($data);
+
+        }
     }
 
     /**
